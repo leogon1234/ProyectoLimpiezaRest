097309f47fc37c17b96b1b9a00d4cdb4bb4793a1
leogon1234 <leandrogv2105@gmail.com>
2025-11-30 00:17:24 -0300

    Update config.java
    se cambio el localhost a 5173

--- LimpiFresh/src/main/java/ProyectoLimpiFreshRest/LimpiFresh/Config/config.java
+++ LimpiFresh/src/main/java/ProyectoLimpiFreshRest/LimpiFresh/Config/config.java
@@ -14,7 +14,7 @@ public class config {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5175")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
